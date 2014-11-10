a93deabedfb5a66fecba4b16554e0e447a7c7065
akht <picopeko@gmail.com>
2014-11-10 20:31:35 +0900

    second commit

--- myanki.py
+++ myanki.py
@@ -5,10 +5,14 @@ import sys
 import os
 import random
 
+# 一度に出題する数
 NUM = 10
 
-
 options = sys.argv[1:]
+
+# 出題するためのファイルを決める
+# デフォルトではfiles/からランダムで選ぶ
+# オプションで指定されていればそれを選ぶ
 dir = './files/'
 filename = random.choice(os.listdir(dir))
 pattern = '^[0-9]{1,2}$'
@@ -18,21 +22,28 @@ for i in options:
 
 target_file = dir + filename
 
+
+# ファイル中のセンテンスの出題方法
+# not random_mode(デフォ) = 上から順に出題
+# random_mode = 順番関係なくランダムに出題
 random_mode = False
 if 'r' in options:
     random_mode = True
 
 
-
+# 選択されたファイルから、
+# 英語文と日本語文がペアになったリストを作る
 item_list = []
 with open(target_file, 'r') as f:
     for i in range(NUM):
         line = f.readline()
         item_list.append(line[:-1].split('\t'))
 
-        if random_mode:
-            random.shuffle(item_list)
+if random_mode:
+    random.shuffle(item_list)
 
+
+# 出題
 for i in range(NUM):
     count = 0
     question = item_list[i][0]
@@ -48,10 +59,7 @@ for i in range(NUM):
             if count < 2:
                 print('✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗')
             else:
-                print('==========正解は・・・========')
+                print('==========正解は========')
                 print(correct_sentence)
-                print('~~~~~~2回入力して覚えよう~~~~~~')
-
-                for i in range(2):
-                    input()
-                break
+                print('~~~~~~入力して覚えよう~~~~~~')
+                input()
