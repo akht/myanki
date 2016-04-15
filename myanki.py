eb7e64c4220cdca1f0146a2c18172f2e4807b83f
akht <picopeko@gmail.com>
2016-04-16 03:52:52 +0900

    Refactored the whole code from a backup:innocent:

--- myanki.py
+++ myanki.py
@@ -1,28 +1,29 @@
-#!/user/bin/env python3
 # coding: utf-8
 
 import sys
 import os
 import random
-import re
+
+
+class EnglishSentence:
+    def __init__(self, sentence):
+        self.sentence = sentence
+
+    # case-insensitive
+    def is_equivalent(self, target_sentence):
+        if self.sentence.lower() == target_sentence.lower():
+            return True
+        else:
+            return False
 
 # 一度に出題する数
 NUM = 10
-
 options = sys.argv[1:]
 
-# 出題するためのファイルを決める
-# デフォルトではres/からランダムで選ぶ
-# オプションで指定されていればそれを選ぶ
-dir = './res/'
-filename = random.choice(os.listdir(dir))
-pattern = '^[0-9]{1,2}$'
-for i in options:
-    if re.match(pattern, i):
-        filename = i + '.txt'
-
-target_file = dir + filename
-
+# 出題に使うファイルを決める
+res_dir = './res/'
+filename = random.choice(os.listdir(res_dir))
+target_file = res_dir + filename
 
 # ファイル中のセンテンスの出題方法
 # not random_mode(デフォ) = 上から順に出題
@@ -45,23 +46,28 @@ if random_mode:
 
 
 # 出題
+print('')
+print(str(filename))
+print('')
 for i in range(NUM):
     count = 0
     question = item_list[i][0]
-    correct_sentence = item_list[i][1]
+    correct = EnglishSentence(item_list[i][1])
+
     while True:
         count += 1
         print('[ja]-> ', question)
-        user_answer = input('[en]-> ')
-        if user_answer == correct_sentence:
-            print('✔✔✔✔✔✔✔✔✔✔✔✔✔✔✔')
+        user_input = input('[en]-> ')
+        user_answer = EnglishSentence(user_input)
+        if user_answer.is_equivalent(correct.sentence):
+            print('\n(˵¯͒〰¯͒˵) Good! (˵¯͒〰¯͒˵)\n')
             break
         else:
             if count < 2:
-                print('✗✗✗✗✗✗✗✗✗✗✗✗✗✗✗')
+                print('\nʕ ͠° ʖ̫ °͠ ʔ Oops! ʕ ͠° ʖ̫ °͠ ʔ\n')
             else:
-                print('==========正解は========')
-                print(correct_sentence)
-                print('~~~~~~入力して覚えよう~~~~~~')
+                print('~~~~正解は~~~~')
+                print(correct.sentence)
+                print('~~入力して覚えよう~~')
                 input()
                 break
